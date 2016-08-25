40611373f2c90299cb64ab410bc26055d99f49fa
Sebastian Gjertsen <TheDude@Sebastians-MacBook-Pro.local>
2016-08-25 18:04:22 +0200

    kjører men feil lift og drag

--- von_karman_fsi_fluid.py
+++ von_karman_fsi_fluid.py
@@ -11,13 +11,11 @@ VVQ = MixedFunctionSpace([V1,V2,Q])
 
 # BOUNDARIES
 
-#NOS = AutoSubDomain(lambda x: "on_boundary" and( near(x[1],0) or near(x[1], 0.41)))
 Inlet = AutoSubDomain(lambda x: "on_boundary" and near(x[0],0))
 Outlet = AutoSubDomain(lambda x: "on_boundary" and (near(x[0],2.5)))
 Bar = AutoSubDomain(lambda x: "on_boundary" and (near(x[1], 0.21)) or near(x[1], 0.19) or near(x[0], 0.6 ) )
-Bar2 = AutoSubDomain(lambda x: "on_boundary" and  (0.18 < x[1] < 0.22))
 Circle =  AutoSubDomain(lambda x: "on_boundary" and (( (x[0] - 0.2)*(x[0] - 0.2) + (x[1] - 0.2)*(x[1] - 0.2)  < 0.0505*0.0505 )  ))
-Circle2 =  AutoSubDomain(lambda x: "on_boundary" and (( (x[0] - 0.2)*(x[0] - 0.2) + (x[1] - 0.2)*(x[1] - 0.2)  < 0.0505*0.0505 )  and x[1]>=0.19 and x[1]<=0.21 and x[0]>0.2 ))
+BarLeftSide =  AutoSubDomain(lambda x: "on_boundary" and (( (x[0] - 0.2)*(x[0] - 0.2) + (x[1] - 0.2)*(x[1] - 0.2)  < 0.0505*0.0505 )  and x[1]>=0.19 and x[1]<=0.21 and x[0]>0.2 ))
 
 test = DomainBoundary()
 
@@ -26,8 +24,9 @@ boundaries.set_all(0)
 test.mark(boundaries, 1)
 Inlet.mark(boundaries, 2)
 Outlet.mark(boundaries, 3)
-#Circle2.mark(boundaries, 10)
+BarLeftSide.mark(boundaries, 5)
 Bar.mark(boundaries, 4)
+
 
 ds = Measure("ds", subdomain_data = boundaries)
 dS = Measure("dS", subdomain_data = boundaries)
@@ -42,19 +41,18 @@ inlet = Expression(("1.5*Um*4.0*x[1]*(H-x[1])/pow(H/2.0,2)","0"),t=0.0,Um = Um,H
 u_inlet = DirichletBC(VVQ.sub(0), (inlet), boundaries, 2)
 nos = DirichletBC(VVQ.sub(0), ((0, 0)), boundaries, 1)
 bar = DirichletBC(VVQ.sub(0), ((0, 0)), boundaries, 4)
-bcs = [nos,u_inlet,bar]
+bs1 = DirichletBC(VVQ.sub(1), ((0, 0)),boundaries , 5) # left side bar no deformation
+bcs = [nos,u_inlet,bar,bc1]
 
 # AREAS
 
-Fluid_ = AutoSubDomain(lambda x: x[0]>=0 ) # The entire domain, then overriden
 Bar_area = AutoSubDomain(lambda x: (0.19 <= x[1] <= 0.21) and 0.24<= x[0] <= 0.6) # only the "flag" or "bar"
 
 domains = CellFunction("size_t",mesh)
-domains.set_all(0)
-Fluid_.mark(domains,1)
+domains.set_all(1)
 Bar_area.mark(domains,2)
 dx = Measure("dx",subdomain_data=domains)
-plot(domains,interactive = True)
+#plot(domains,interactive = True)
 
 
 # TEST TRIAL FUNCTIONS
@@ -74,8 +72,8 @@ k = Constant(dt)
 rho_f = 1.0
 mu_f = 1.0
 lamda = Constant("0.0105e9")
-mu_s = Constant("1.0e1")
-rho_s =Constant("1.0e1")
+mu_s = Constant("1.0e12")
+rho_s =Constant("1.0e6")
 
 
 def sigma_structure(d):
@@ -92,13 +90,13 @@ F = rho_f*((1./k)*inner(u-u1,phi)*dx(1) \
 # Structure Variational form
 U = U1 + w*k
 
-G = rho_s*((1./k)*inner(w-w0,psi))*dx(2) + inner(sigma_structure(U),grad(psi))*dx(2) \
-+ inner(u,phi)*dx(2) - inner(w,phi)*dx(2)
+G = rho_s*((1./k)*inner(w-w0,psi))*dx(2) + inner(sigma_structure(U),grad(psi))*dx(2)
 
 # Mesh movement, solving the equation laplace -nabla(grad(d))
 
-H = inner(grad(U),grad(psi))*dx(2) - inner(grad(U("-"))*n("-"),psi("-"))*dS(4)
+H = inner(grad(U),grad(psi))*dx(1) - inner(grad(U("-"))*n("-"),psi("-"))*dS(4)
 
+print assemble(n*Constant((0,1))*dS(4))
 
 a = lhs(F) - lhs(G) - lhs(H)
 L = rhs(F) - rhs(G) - rhs(H)
@@ -133,7 +131,7 @@ while t < T:
     ALE.move(mesh,w_)
     mesh.bounding_box_tree().build(mesh)
 
-    plot(u_)
+    #plot(u_)
 
     u1.assign(u_)
     u_file << u_
