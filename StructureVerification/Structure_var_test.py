9bebaad62eb3eb25b3343e3b6f673d658f282f3c
Sebastian Gjertsen <TheDude@Sebastians-MacBook-Pro.local>
2016-09-15 16:10:38 +0200

    structure var test done, but not right conv-rate

--- StructureVerification/Structure_var_test.py
+++ StructureVerification/Structure_var_test.py
@@ -1,86 +1,88 @@
 from dolfin import *
 import numpy as np
-def solver_structure(N):
+from structur_sympy import *
+set_log_active(False)
+def solver_structure(N,dt):
     mesh = UnitSquareMesh(N,N)
+    mesh2 = UnitSquareMesh(100,100)
     V = VectorFunctionSpace(mesh,"CG",1)
+    V2 = VectorFunctionSpace(mesh2,"CG",2)
 
-    u = Function(V)
     psi = TestFunction(V)
-    u1 = Function(V)
-    bcs = []
+    u = Function(V)
+    #u0 = Function(V)
+    #u1 = Function(V)
+    class On(SubDomain):
+    	def inside(self, x, on_boundary):
+    		return on_boundary
+    on = On()
+    boundaries = FacetFunction("size_t",mesh)
+    boundaries.set_all(0)
+    on.mark(boundaries,1)
+    u_0 = Expression(("x[0]*x[0]","x[1]*x[1]"))
+    bc1 = DirichletBC(V,u_0, boundaries,1)
+    bcs = [bc1]
+    #plot(boundaries,interactive=True)
 
-    rho_s = 1.0E3
     mu_s = 0.5E6
     nu_s = 0.4
+    rho_s = 1.0E3
     lamda = nu_s*2*mu_s/(1-2*nu_s)
 
-    I = Expression(("x[0]*x[0]+t*t","x[1]*x[1]+t*t"),t=0)
-    u0 = Function(V)
-    #f = Expression(("(2*x[0]+t*t)*(mu+0.5)+mu*(x[1]+t*t)+2","mu*(x[0]+t*t)+(mu+0.5*lam)*(2*x[1]+2*t)+2 "),mu=mu_s,lam=lamda,rho_s=rho_s,t=0)
-    #f = Expression(("rho_s*2","rho_s*2"),rho_s=rho_s)
-    f = Expression(("-24*lamda*x[0]*x[0] - 24*lamda*x[0] - 8*lamda*x[1]*x[1] - 8*lamda*x[1] - 4*lamda - 16*mu_s*x[0] - 8*mu_s + 2","-8*lamda*x[0]*x[0] - 8*lamda*x[0] - 24*lamda*x[1]*x[1] - 24*lamda*x[1] - 4*lamda - 16*mu_s*x[1] - 8*mu_s + 2"),mu_s =mu_s, lamda=lamda)
+    I_1,f = find_my_f()
 
-    dt = 0.001
+    #    print I_1, f
+
+    #I_1 = Expression(("x[0]*x[0]+t*t","x[1]*x[1]+t*t"),t=0)
+    u1=interpolate(I_1,V)
+    I_1.t = dt
+    u0 = interpolate(I_1,V)
+
+
     k = Constant(dt)
 
     def s_s_n_l(U):
         I = Identity(2)
         F = I + grad(U)
         E = 0.5*((F.T*F)-I)
-        return lamda*tr(E)*I + 2*mu_s*E
+        return F*(lamda*tr(E)*I + 2*mu_s*E)
 
-    G =rho_s*((1./k**2)*inner(u - 2*u0 + u1,psi))*dx + inner(s_s_n_l(u0),grad(psi))*dx - inner(f,psi)*dx
+    G =rho_s*((1./k**2)*inner(u - 2*u0 + u1,psi))*dx + inner(s_s_n_l(0.5*(u+u0)),grad(psi))*dx - inner(f,psi)*dx
 
     T = 1.0
-    t=dt
+    t=2*dt
     counter = 0
-    e_list = np.zeros(10000)
+    e_list = []
     while t<=T:
-        I.t = t
-        u_ = interpolate(I,V)
+        I_1.t = t
+        u_ = interpolate(I_1,V2)
+        #plot(u_)
         f.t = t
         print "Timestep: ",t
         solve(G==0,u,bcs,solver_parameters={"newton_solver": \
         {"relative_tolerance": 1E-6,"absolute_tolerance":1E-6,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        #plot(u)
+        u_new = interpolate(u,V2)
+        e_list.append(errornorm(u_, u_new, norm_type="l2",degree_rise=3))
+
         u0.assign(u1)
         u1.assign(u)
-        e_list[counter] = errornorm(u, u_, norm_type="l2",degree_rise=3)
-        print e_list[counter]
+        #print e_list[counter]
         t += dt
         counter += 1
-    return e_list, mesh.hmin()
+    return np.mean(e_list), mesh.hmin()
 
 
-print solver_structure(100)
+#solver_structure(200)
 
 
-"""
-N_list = [8,16,32,64]
+
+N_list = [2,4,8,16,32]
 E = np.zeros(len(N_list))
 h = np.zeros(len(N_list))
-for j in range(4):
-    E[j],h[j]=solver_structure(N_list[j])
-print E, h
+for j in range(len(N_list)):
+    E[j],h[j]=solver_structure(N_list[j],dt=0.1)
+    print "Error: %2.2E ,N: %.d: "%(E[j],N_list[j])
 for i in range(1, len(E)):
-    r = ln(E[i]/E[i-1])/ln(h[i]/h[i-1])
-    print "h=%10.2E r=%.2f" % (h[i], r)"""
-
-"""
-for i in range(len(N)):
-    u ,u_exact,V,mesh = solver_1(N[i],mu,beta=1.0)
-    #ex_norm = assemble((u_exact.dx(0) - u.dx(0))**2*dx)
-    #ey_norm = assemble((u_exact.dx(1) - u.dx(1))**2*dx)
-
-    #e = sqrt(mesh.hmin()*ex_norm + mu*(ex_norm+ey_norm))
-    e = errornorm(u_exact, u, norm_type = "l2",degree_rise = 3)
-    print e
-    #u_norm = norm(u_exact,"H1")
-    #e = errornorm(u_exact,u,norm_type = "l2",degree_rise = 3)
-    b[i] = np.log(e)#/u_norm)
-    a[i] = np.log(mesh.hmin())#1./(N[i]))
-    #print u_norm
-    #u_norm = sum(u_ex*u_ex)**0.5
-A = np.vstack([a,np.ones(len(a))]).T
-alpha_1,c_1 = np.linalg.lstsq(A,b)[0]
-
-print "mu = ",mu, "C =  ", exp(c_1), "alpha =  ", alpha_1"""
+    r = np.log(E[i]/E[i-1])/np.log(h[i]/h[i-1])
+    print "h=%10.2E r=%.6f" % (h[i], r)
--- StructureVerification/structur_sympy.py
+++ StructureVerification/structur_sympy.py
@@ -1,30 +1,88 @@
-from sympy import *
+from dolfin import *
+import numpy as np
+import sympy as sp
+from sympy.printing.ccode import CCodePrinter
 
-x,y,t,rho_s, mu_s,lamda = symbols('x[0] x[1] t rho_s mu_s lamda')
+def is_sympy(args):
+    'Check if args are sympy objects.'
+    try:
+        return args.__module__.split('.')[0] == 'sympy'
+    except AttributeError:
+        return False
+class DolfinCodePrinter(CCodePrinter):
+    '''
+    This class provides functionality for converting sympy expression to
+    DOLFIN expression. Core of work is done by dolfincode.
+    '''
+    def __init__(self, settings={}):
+        CCodePrinter.__init__(self)
 
-u = x**2 + t**2
-v = y**2 + t**2
+    def _print_Pi(self, expr):
+        return 'pi'
 
-ux = diff(u,x)
-uy = diff(u,y)
-vx = diff(v,x)
-vy = diff(v,y)
+def dolfincode(expr, assign_to=None, **settings):
+    # Handle scalars
+    if is_sympy(expr):
+        dolfin_xs = sp.symbols('x[0] x[1] x[2]')
+        xs = sp.symbols('x y z')
+        for x, dolfin_x in zip(xs, dolfin_xs):
+            expr = expr.subs(x, dolfin_x)
+        return DolfinCodePrinter(settings).doprint(expr, assign_to)
 
-# x-direction:
+    # Recurse if vector or tensor
+    elif type(expr) is tuple:
+        return tuple(dolfincode(e, assign_to, **settings) for e in expr)
+def find_my_f():
+    x,y,t,mu_s,rho_s,lamda = sp.symbols('x y t mu_s rho_s lamda')
 
-utt = diff(u,t,t) # second derivative
-Fx = ux + 1 + vx
-trE = (ux+1)**2+vx**2-1 + uy**2+(vy+1)**2-1
-Ex = (ux+1)**2 + (vx)**2 -1 + (ux+1)*uy+vy*(vy+1)
-sigma_x = Fx*lamda*trE + 2*mu_s*Ex
-F1 =utt- diff(sigma_x,x)
-print simplify(F1)
+    #u = x**2 + t**2
+    #v = y**2 + t**2
+    u = sp.sin(2*sp.pi*t) + sp.sin(2*sp.pi*x)*sp.sin(2*sp.pi*y)
+    v = sp.cos(2*sp.pi*t) + sp.cos(2*sp.pi*x)*sp.cos(2*sp.pi*y)
+    var = [u,v]
+    #u = sin(2*pi*t) + sin(2*pi*x)*sin(2*pi*y)
+    #v = cos(2*pi*t) + cos(2*pi*x)*cos(2*pi*y)
+
+    ux = sp.diff(u,x)
+    uy = sp.diff(u,y)
+    vx = sp.diff(v,x)
+    vy = sp.diff(v,y)
+
+    # x-direction:
+
+    utt = sp.diff(u,t,t) # second derivative
+    Fx = ux + 1 + vx
+    trE = 0.5*((ux+1)**2+vx**2 - 1 + uy**2+(vy+1)**2-1)
+    Ex = 0.5*((ux+1)**2 + (vx)**2 - 1 + (ux+1)*uy+vx*(vy+1))
+    sigma_x = Fx*(lamda*trE + 2*mu_s*Ex)
+    F1 =rho_s*utt - sp.diff(sigma_x,x)
+    #print simplify(F1)
 
 
-# y-direction:
-vtt = diff(v,t,t) # second derivative
-Fy = vy + 1 + uy
-Ey = uy*(ux+1)+(vy+1)*vx + uy**2+(vy+1)**2-1
-sigma_y = Fy*lamda*trE + 2*mu_s*Ey
-F2 = vtt - diff(sigma_y,y)
-print simplify(F2)
+    # y-direction:
+    vtt = sp.diff(v,t,t) # second derivative
+    Fy = vy + 1 + uy
+    Ey = 0.5*(uy*(ux+1)+(vy+1)*vx + uy**2+(vy+1)**2-1)
+    sigma_y = Fy*(lamda*trE + 2*mu_s*Ey)
+    F2 = rho_s*vtt - sp.diff(sigma_y,y)
+    #print simplify(F2)
+    f = (F1, F2)
+    f = dolfincode(f)
+    # Expression for the source term in the MMS
+    mu_s = 0.5E6
+    nu_s = 0.4
+    rho_s = 1.0E3
+    lamda = nu_s*2*mu_s/(1-2*nu_s)
+    t = 0
+    f = Expression(f, mu_s=mu_s,lamda=lamda,rho_s=rho_s,t=t)
+     # Expression for the velocity components
+    u_ = []
+    for var_ in var:
+        u_.append(dolfincode(var_))
+
+    exact_u = Expression(tuple(u_),t=t)
+
+
+    return exact_u,f
+if __name__ == "__main__":
+    exact_u,f= find_my_f()
