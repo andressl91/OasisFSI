8868e7f1ad9c9212da8d9dbc5dee07e1336056c3
Sebastian Gjertsen <TheDude@Sebastians-MacBook-Pro.local>
2016-09-29 13:04:32 +0200

    fixed with if tests

--- StructureVerification/von_karman_structure.py
+++ StructureVerification/von_karman_structure.py
@@ -1,19 +1,21 @@
 from dolfin import *
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 set_log_active(False)
+implementation = sys.argv[1]
+print implementation
 
 mesh = Mesh("von_karman_street_FSI_structure.xml")
-#print mesh.coordinates()
+
 for coord in mesh.coordinates():
-    if coord[0]==0.6 and (0.199<=coord[1]<=0.2001):
+    if coord[0]==0.6 and (0.199<=coord[1]<=0.2001): # to get the point [0.2,0.6] end of bar
         print coord
         break
 
-#plot(mesh,interactive=True)
-
-V = VectorFunctionSpace(mesh, "CG", 1) # Mesh movement
-#VV=V*V
+V = VectorFunctionSpace(mesh, "CG", 1)
+if implementation == "2":
+    VV=V*V
 print "Dofs: ",V.dim(), "Cells:", mesh.num_cells()
 
 BarLeftSide =  AutoSubDomain(lambda x: "on_boundary" and (( (x[0] - 0.2)*(x[0] - 0.2) + (x[1] - 0.2)*(x[1] - 0.2)  < 0.0505*0.0505 )  and x[1]>=0.19 and x[1]<=0.21 and x[0]>0.2 ))
@@ -22,9 +24,6 @@ boundaries = FacetFunction("size_t",mesh)
 boundaries.set_all(0)
 BarLeftSide.mark(boundaries,1)
 #plot(boundaries,interactive=True)
-bc1 = DirichletBC(V, ((0,0)),boundaries, 1)
-#bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
-bcs = [bc1]#,bc2]
 
 #PARAMETERS:
 rho_s = 1.0E3
@@ -32,51 +31,61 @@ mu_s = 0.5E6
 nu_s = 0.4
 E_1 = 1.4E6
 lamda = nu_s*2*mu_s/(1-2*nu_s)
+g = Constant((0,-2*rho_s))
+dt = 0.02
+k = Constant(dt)
 
 #Hookes
-def sigma_structure(U):
-    return 2*mu_s*sym(grad(U)) + lamda*tr(sym(grad(U)))*Identity(2)
+def sigma_structure(d):
+    return 2*mu_s*sym(grad(d)) + lamda*tr(sym(grad(d)))*Identity(2)
+
 #Second piola stress
-def s_s_n_l(U):
+def s_s_n_l(d):
     I = Identity(2)
-    F = I + grad(U)
+    F = I + grad(d)
     E = 0.5*((F.T*F)-I)
     #J = det(F)
     return lamda*tr(E)*I + 2*mu_s*E
 
 
 
-# TEST TRIAL FUNCTIONS
-
-#psi,phi = TestFunctions(VV)
-#wU = Function(VV)
-#w,U = split(wU)
-#U = Function(V)
-#U0 = Function(V)
-#w0U0 = Function(VV)
-#w0,U0 = split(w0U0)
-psi = TestFunction(V)
-w = Function(V)
-w0 = Function(V)
-w1 = Function(V)
-
-dt = 0.02
-k = Constant(dt)
-
-#Structure Variational form
-g = Constant((0,-2*rho_s))
- #Fungerer
-
-#Variational form with double spaces
-
-#G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx + inner(s_s_n_l(0.5*(U+U0)),grad(psi))*dx \
-#- inner(g,psi)*dx - dot(U-U0,phi)*dx + k*dot(0.5*(w+w0),phi)*dx
-
 
 #Variational form with single space, just solving displacement
+if implementation =="1":
+    bc1 = DirichletBC(V, ((0,0)),boundaries, 1)
+    bcs = [bc1]
+    psi = TestFunction(V)
+    d = Function(V)
+    d0 = Function(V)
+    d1 = Function(V)
+    G =rho_s*((1./k**2)*inner(d - 2*d0 + d1,psi))*dx \
+    + inner(s_s_n_l(0.5*(d+d1)),grad(psi))*dx - inner(g,psi)*dx
 
-G =rho_s*((1./k**2)*inner(w - 2*w0 + w1,psi))*dx + inner(s_s_n_l(0.5*(w+w1)),grad(psi))*dx \
-- inner(g,psi)*dx #+ rho_s*inner(dot(grad(W), W),psi)*dx #+ dot(U-U0,phi)*dx - k*dot(0.5*(w+w0),phi)*dx
+#Variational form with double spaces
+elif implementation =="2":
+    bc1 = DirichletBC(VV.sub(0), ((0,0)),boundaries, 1)
+    bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
+    bcs = [bc1,bc2]
+    psi,phi = TestFunctions(VV)
+    wd = Function(VV)
+    w,d = split(wd)
+    w0d0 = Function(VV)
+    w0,d0 = split(w0d0)
+
+    G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx + inner(s_s_n_l(0.5*(d+d0)),grad(psi))*dx \
+    - inner(g,psi)*dx - dot(d-d0,phi)*dx + k*dot(0.5*(w+w0),phi)*dx
+elif implementation == "3":
+    bc1 = DirichletBC(V, ((0,0)),boundaries, 1)
+    bcs = [bc1]
+    psi = TestFunction(V)
+    w = Function(V)
+    w0 = Function(V)
+    d0 = Function(V)
+    d = d0 + w*k
+    G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx \
+    + inner(s_s_n_l(0.5*(d+d0)),grad(psi))*dx - inner(g,psi)*dx
+
+
 
 
 dis_x = []
@@ -90,32 +99,41 @@ print "time len",len(time)
 from time import sleep
 while t < T:
     print "Time: ",t
-    ##J = derivative(G,w)
-    solve(G == 0, w, bcs, solver_parameters={"newton_solver": \
-    {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
-
-    #w0U0.assign(wU)
-    #w,U=split(wU)
-    #w,U = wU.split()
-    w1.assign(w0)
-    w0.assign(w)
-    #U0.assign(U)
-
-    #ALE.move(mesh, w)
-    #mesh.bounding_box_tree().build(mesh)
-    #plot(U,mode="displacement")
-    #w.vector()[:] *= float(k)
-    #U0.vector()[:] += w.vector()[:]
-    #plot(U,mode="displacement")
-    #sleep(0.2)
-    #print "Ux: %1.4e , Uy: %2.4e "%(U1(coord)[0],U1(coord)[1])
+    if implementation == "1":
+        solve(G == 0, d, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        d1.assign(d0)
+        d0.assign(d)
+        #plot(d,mode="displacement")
+        dis_x.append(d(coord)[0])
+        dis_y.append(d(coord)[1])
+    elif implementation == "2":
+        solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        w0d0.assign(wd)
+        w,d = wd.split()
+        #plot(d,mode="displacement")
+        dis_x.append(d(coord)[0])
+        dis_y.append(d(coord)[1])
+    elif implementation == "3":
+        solve(G == 0, w, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        w0.assign(w)
+        d0.assign(d)
+        #plot(d0,mode="displacement")
+        dis_x.append(d0(coord)[0])
+        dis_y.append(d0(coord)[1])
     t += dt
-    dis_x.append(w(coord)[0])
-    dis_y.append(w(coord)[1])
 
-#print "Ux: %1.4e , Uy: %2.4e "%(U(coord)[0],U(coord)[1])
 print len(dis_x), len(time)
-plt.plot(time,dis_x,);plt.title("Single space"); plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
+if implementation == "1":
+    title = plt.title("Single space solving d")
+elif implementation == "2":
+    title = plt.title("Double space")
+elif implementation == "3":
+    title = plt.title("Single space solving w")
+
+plt.plot(time,dis_x,);title; plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
 plt.show()
-plt.plot(time,dis_y);plt.title("Single space");plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
+plt.plot(time,dis_y);title;plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
 plt.show()
