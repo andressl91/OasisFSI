ef0e393b9a0ea3ffed49e67806484022ee8ce089
andressl91 <andressl@math.uio.no>
2016-10-06 07:13:42 +0000

    Full Eulerian implemented, but with damping

--- StructureVerification/compare.py
+++ StructureVerification/compare.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 set_log_active(False)
-"""if len(sys.argv) != 2:
-    print "Usage: %s [implementation (1/2/3)]" % sys.argv[0]
-    sys.exit(0)"""
 implementation = sys.argv[1]
 print implementation
 
@@ -17,7 +14,7 @@ for coord in mesh.coordinates():
         break
 
 V = VectorFunctionSpace(mesh, "CG", 1)
-if implementation == "2" or "4":
+if implementation == "2" or "3":
     VV=V*V
 print "Dofs: ",V.dim(), "Cells:", mesh.num_cells()
 
@@ -35,7 +32,8 @@ nu_s = 0.4
 E_1 = 1.4E6
 lamda = nu_s*2*mu_s/(1-2*nu_s)
 g = Constant((0,-2*rho_s))
-dt = float(sys.argv[2])
+dt = float(sys.argv[2]) #FOR FASTER TESTNING!! REMOVE AFTER TESTS!!!!
+#dt = 0.02
 k = Constant(dt)
 
 #Hookes
@@ -57,20 +55,23 @@ def Venant_Kirchhof(d):
     E = 0.5*((inv(F.T)*inv(F))-I)
     return J*inv(F)*(2.*mu_s*E + lamda*tr(E)*I)*inv(F.T)
 
-#Second piola stress
-def s_s_n_l_2(d):
+#First Piola stress tensor P = J*Sigma*F^(-T)
+def Piola1(d):
     I = Identity(2)
     F = I + grad(d)
-    E = 0.5*((F.T*F)-I)
     J = det(F)
-    return (1/J)*F*(lamda*tr(E)*I + 2*mu_s*E)*F.T
+    E = 0.5*((F.T*F)-I)
+    return F*(lamda*tr(E)*I + 2*mu_s*E )
+
+
+
+#Second piola stress S = J*F^(-1)*sigma*F^(-T)
 def s_s_n_l(d):
     I = Identity(2)
     F = I + grad(d)
     E = 0.5*((F.T*F)-I)
     #J = det(F)
     return lamda*tr(E)*I + 2*mu_s*E
-
 
 #Variational form with single space, just solving displacement
 if implementation =="1":
@@ -98,23 +99,8 @@ elif implementation =="2":
     G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx + inner(s_s_n_l(0.5*(d+d0)),grad(psi))*dx \
     - inner(g,psi)*dx - dot(d-d0,phi)*dx + k*dot(0.5*(w+w0),phi)*dx
 
-#Solving for displacement velocity and updating for displacement.
-elif implementation == "3":
-    bc1 = DirichletBC(V, ((0,0)),boundaries, 1)
-    bcs = [bc1]
-    psi = TestFunction(V)
-    w = Function(V)
-    w0 = Function(V)
-    d0 = Function(V)
-    d = Function(V)
-    d = d0 + w*k
-    #d = d0+0.5*(w+w0)*k
-
-    G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx \
-    + inner(s_s_n_l(0.5*(d+d0)),grad(psi))*dx - inner(g,psi)*dx
-
 #Full Eulerian formulation
-elif implementation == "4":
+elif implementation == "3":
     bc1 = DirichletBC(VV.sub(0), ((0,0)),boundaries, 1)
     bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
     bcs = [bc1,bc2]
@@ -134,20 +120,40 @@ elif implementation == "4":
     + inner(Venant_Kirchhof(d), grad(psi)) - J_*inner(g, psi) ) * dx \
     + inner((d - d0) + k*dot(grad(d), w) - k*w, phi) * dx
 
-    #Cranc Nic
+    #Cranc Nic some
     #G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
     #+ inner(Venant_Kirchhof(0.5*(d + d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
     #+ inner(d - d0 + k*dot(grad(d), w) - k*w0 , phi) * dx
 
+    #First order frac-step
+    # solving for n + 1/2
+    """
+    G_1 = ( J_*rho_s/(k/2)*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w0), w0), psi) \
+    + inner(Venant_Kirchhof(d0), grad(psi)) - J_*inner(g, psi) ) * dx \
+    + inner((d - d0) + k/2*dot(grad(d), w) - k/2*w, phi) * dx
+
+    G = ( J_*rho_s/(k/2)*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w), w), psi) \
+    + inner(Venant_Kirchhof(d), grad(psi)) - J_*insner(g, psi) ) * dx \
+    + inner((d - d0) + k/2*dot(grad(d), w) - k/2*w, phi) * dx
+    """
+
 dis_x = []
 dis_y = []
 
-T = 10.0
+T = 5;
 t = 0
 time = np.linspace(0,T,(T/dt)+1)
 print "time len",len(time)
+###### REMOVE BEFORE MERGE
+dis_1 = File("./displace_1/dis.pvd")
+dis_2 = File("./displace_2/dis.pvd")
+dis_3 = File("./displace_3/dis.pvd")
+dis_4 = File("./displace_4/dis.pvd")
+dis_5 = File("./displace_5/dis.pvd")
+dis_6 = File("./displace_6/dis.pvd")
 
 from time import sleep
+count = 0
 while t < T:
     print "Time: ",t
     if implementation == "1":
@@ -155,58 +161,50 @@ while t < T:
         {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
         d1.assign(d0)
         d0.assign(d)
-        plot(d,mode="displacement")
+        dis_1 << d
+        #plot(d,mode="displacement")
         dis_x.append(d(coord)[0])
         dis_y.append(d(coord)[1])
 
     elif implementation == "2":
         solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
-        {"relative_tolerance": 1E-7,"absolute_tolerance":1E-7,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
         w0d0.assign(wd)
-        w0,d0 = w0d0.split(True)
-        w,d = wd.split(True)
+        w,d = wd.split()
+        dis_2 << d
         #plot(d,mode="displacement")
-        #w0.assign(w)
-        #w.vector()[:] *= float(k)
-        #d0.vector()[:] += w.vector()[:]
-        #plot(d,mode="displacement")
-
-        #ALE.move(mesh,w)
-        #mesh.bounding_box_tree().build(mesh)
-        #plot(mesh)
         dis_x.append(d(coord)[0])
         dis_y.append(d(coord)[1])
 
-
     elif implementation == "3":
-        solve(G == 0, w, bcs, solver_parameters={"newton_solver": \
-        {"relative_tolerance": 1E-6,"absolute_tolerance":1E-6,"maximum_iterations":100,"relaxation_parameter":1.0}})
-        w0.assign(w)
-        #d0.assign(d)
-        w.vector()[:] *= float(k)
-        d0.vector()[:] += w.vector()[:]
-        #d0.assign(d)
-        #ALE.move(mesh,w)
-        #mesh.bounding_box_tree().build(mesh)
-        plot(d0,mode="displacement")
-        dis_x.append(d0(coord)[0])
-        dis_y.append(d0(coord)[1])
-        #w0.assign(w)
-
-        #sleep(0.11)
-    elif implementation == "4":
+        #solve(G_1 == 0, wd, bcs, solver_parameters={"newton_solver": \
+        #{"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        #w0d0.assign(wd)
         solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
         {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
         w0d0.assign(wd)
         w0,d0 = w0d0.split(True)
         d_disp.vector()[:] = w0.vector()[:]*float(k)
+        dis_3 << d0
         ALE.move(mesh, d_disp)
         mesh.bounding_box_tree().build(mesh)
 
-        #plot(d0, mode="displacement")
-        dis_x.append(d0(coord)[0])
-        dis_y.append(d0(coord)[1])
+
+    elif implementation == "4":
+        solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        w0d0.assign(wd)
+        w0,d0 = w0d0.split(True)
+        w,d = wd.split(True)
+        dis_4 << d0
+        d_disp.vector()[:] = w0.vector()[:]*float(k)
+        ALE.move(mesh, d_disp)
+        mesh.bounding_box_tree().build(mesh)
+
+
+    count += 1
     t += dt
+
 
 print len(dis_x), len(time)
 if implementation == "1":
@@ -215,12 +213,11 @@ elif implementation == "2":
     title = plt.title("Double space")
 elif implementation == "3":
     title = plt.title("Single space solving w")
+    sys.exit(0)
 elif implementation == "4":
-    title = plt.title("Eulerian MixedFunctionSpace")
+    sys.exit(0)
 
-plt.plot(time,dis_x,);title; plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
-#plt.savefig("run_x.jpg")
-plt.show()
-plt.plot(time,dis_y);title;plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
-#plt.savefig("run_y.jpg")
-plt.show()
+#plt.plot(time,dis_x,);title; plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
+#plt.show()
+#plt.plot(time,dis_y);title;plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
+#plt.show()
