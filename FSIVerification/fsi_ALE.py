f328f84b93683f21d3eb6cbeae3c0a3c0ac95567
Sebastian Gjertsen <TheDude@Sebastians-MacBook-Pro.local>
2016-10-18 13:11:13 +0200

    fsi_ALE showing flow, first time

--- FSIVerification/fsi_ALE.py
+++ FSIVerification/fsi_ALE.py
@@ -30,7 +30,7 @@ Outlet.mark(boundaries, 4)
 Bar.mark(boundaries, 5)
 Circle.mark(boundaries, 6)
 Barwall.mark(boundaries, 7)
-#plot(boundaries,interactive=True)
+plot(boundaries,interactive=True)
 
 
 ds = Measure("ds", subdomain_data = boundaries)
@@ -49,6 +49,8 @@ inlet = Expression(("1.5*Um*x[1]*(H - x[1]) / pow((H/2.0), 2) * (1 - cos(t*pi/2)
 u_inlet  = DirichletBC(VVQ.sub(0), inlet, boundaries, 3)
 u_wall   = DirichletBC(VVQ.sub(0), ((0.0, 0.0)), boundaries, 2)
 u_circ   = DirichletBC(VVQ.sub(0), ((0.0, 0.0)), boundaries, 6) #No slip on geometry in fluid
+u_bar    = DirichletBC(VVQ.sub(0), ((0.0, 0.0)), boundaries, 5) #No slip on geometry in fluid
+
 
 
 #Mesh velocity conditions
@@ -57,6 +59,8 @@ w_inlet   = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 3)
 w_outlet  = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 4)
 w_circle  = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 6)
 w_barwall = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 7)
+w_bar     = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 5)
+
 
 #deformation condition
 #d_barwall = DirichletBC(VVQ.sub(2), ((0, 0)), boundaries, 7)
@@ -65,8 +69,8 @@ w_barwall = DirichletBC(VVQ.sub(1), ((0.0, 0.0)), boundaries, 7)
 p_out = DirichletBC(VVQ.sub(2), 0, boundaries, 4)
 
 #Assemble boundary conditions
-bcs = [u_inlet, u_wall, u_circ,  \
-       w_wall, w_inlet, w_outlet,w_barwall,w_circle,\
+bcs = [u_inlet, u_wall, u_circ, u_bar,  \
+       w_wall, w_inlet, w_outlet, w_barwall, w_circle, w_bar,\
        p_out]
 # AREAS
 
@@ -83,7 +87,7 @@ dx_s = dx(2,subdomain_data=domains)
 # TEST TRIAL FUNCTIONS
 phi, psi, gamma = TestFunctions(VVQ)
 #u, w, p = TrialFunctions(VVQ)
-uwp = Function(VVQ)
+uwp = TrialFunction(VVQ)
 u, w, p  = split(uwp)
 
 #uwp0 = Function(VVQ)
@@ -116,40 +120,41 @@ def s_s_n_l(U):
     I = Identity(2)
     F = I + grad(U)
     E = 0.5*((F.T*F)-I)
-    return lamda_s*tr(E)*I + 2*mu_s*E
+    return F*(lamda_s*tr(E)*I + 2*mu_s*E)
 
 def sigma_fluid(p, u): #NEWTONIAN FLUID
     return -p*Identity(2) + 2*mu_f * sym(grad(u))
+def sigma_structure(d): #NEWTONIAN FLUID
+    return 2*mu_s*sym(grad(d)) + lamda_s*tr(sym(grad(d)))*Identity(2)
 
-delta = 1.0
+delta = 0.1
 d = d0 + k*u
 
 # Fluid variational form
-F_fluid =  (rho_f/k)*inner(u -u0,phi)*dx_f + rho_f*inner(dot(grad(u),(u - w)), phi)*dx_f \
-+ inner(sigma_fluid(p, u), grad(phi))*dx_f \
-- inner(div(u),gamma)*dx_f
-
-# Structure Variational form
-F_structure = (rho_s/k)*inner(u-u0,phi)*dx_s + rho_s*inner(dot(grad(0.5*(u+u0)),0.5*(u+u0)),phi)*dx_s  \
- + inner(0.5*(s_s_n_l(d)+s_s_n_l(d0)),grad(phi))*dx_s
+F_fluid =  (rho_f/k)*inner(u - u0,phi)*dx_f + rho_f*inner(dot(grad(u0),(u - w)), phi)*dx_f \
+-inner(p,div(phi))*dx_f + mu_f*inner(grad(u),grad(phi))*dx_f + inner(div(u),gamma)*dx_f #+ inner(dot(sigma_fluid(p,u),n),phi)*ds(5)
 
 #Laplace
-F_laplace = k*inner(grad(w),grad(psi))*dx(1)-inner(grad(d0),grad(psi))*dx_f \
+F_laplace = k*inner(grad(w),grad(psi))*dx_f-inner(grad(d0),grad(psi))*dx_f \
 	       - k*inner(grad(w('-'))*n('-'),psi('-'))*dS(5)\
 	       + inner(grad(d0('-'))*n('-'),psi('-'))*dS(5)
+
+# Structure Variational form
+F_structure = (rho_s/k)*inner(u-u0,phi)*dx_s + rho_s*inner(dot(grad(u),u0),phi)*dx_s  \
+ + inner(sigma_structure(d0),grad(phi))*dx_s #+ inner((s_s_n_l(d0)+k*s_s_n_l(w))*n,phi)*ds(5)
+
+
 
 #Displacement velocity is equal to mesh velocity on dx_s
 F_last = (1./delta)*inner(u,psi)*dx_s - (1./delta)*inner(w,psi)*dx_s
 
-
-
 F = F_fluid + F_structure  + F_laplace + F_last
 
-T = 0.1
+T = 10.0
 t = 0.0
 
-#u_file = File("mvelocity/velocity.pvd")
-#u_file << u0
+u_file = File("mvelocity/velocity.pvd")
+u_file << u0
 
 while t <= T:
     if MPI.rank(mpi_comm_world()) == 0:
@@ -161,21 +166,22 @@ while t <= T:
         inlet.t = 2;
 
     #J = derivative(F, uwdp)
-
-    solve(F == 0, uwp, bcs, solver_parameters={"newton_solver": \
-    {"relative_tolerance": 1E-8,"absolute_tolerance":1E-8,"maximum_iterations":100,"relaxation_parameter":1.0}})
+    uwp1 = Function(VVQ)
+    solve(lhs(F) == rhs(F), uwp1, bcs)#, solver_parameters={"newton_solver": \
+    #{"relative_tolerance": 1E-8,"absolute_tolerance":1E-8,"maximum_iterations":100,"relaxation_parameter":0.5}})
 
 
     #dis_x.append(d(coord)[0])
     #dis_y.append(d(coord)[1])
 
-    u_, w_, p_ = uwp.split(True)
+    u_, w_, p_ = uwp1.split(True)
     w_.vector()[:] *= float(k)
     d0.vector()[:] += w_.vector()[:]
     ALE.move(mesh,w_)
     mesh.bounding_box_tree().build(mesh)
-    plot(u_,mode="displacement")
-    u0.assign(u_)
+    #plot(u_,mode="displacement")
+    #u0.assign(u_)
+    u_file <<u_
     #dis_x.append(d0(coord)[0])
     #dis_y.append(d0(coord)[1])
 
