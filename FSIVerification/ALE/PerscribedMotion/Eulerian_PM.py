cd06975c4f49ec55a4d4f2133019b5ca3a0c063a
Sebastian Gjertsen <TheDude@1x-193-157-243-233.uio.no>
2016-11-10 11:47:57 +0100

    started eulerian_pm

--- FSIVerification/ALE/PerscribedMotion/Eulerian_PM.py
+++ FSIVerification/ALE/PerscribedMotion/Eulerian_PM.py
@@ -37,15 +37,7 @@ class U_bc(Expression):
     def init(self,w):
         self.w = w
     def eval(self,values,x):
-        #x_value, y_value = self.w(x)
-        #values[0] = x_value
-        #values[1] = y_value
         #x_value, y_value = self.w.vector()[[x[0], x[1]]]
-        #values[0] = x_value
-        #values[1] = y_value
-        #print "y-values: ",value[1]
-        #print "x_values: ",value[0]
-        #p = Point(x)
         x_value, y_value = self.w(Point(x))
         values[0] = x_value
         values[1] = y_value
@@ -95,14 +87,6 @@ def sigma_fluid(p, u): #NEWTONIAN FLUID
     #F_ = I + grad(u)
     return -p*Identity(2) + mu_f *(grad(u)+grad(u).T)
 
-def eps(u):
-    return sym(grad(u))
-
-#d = k*w_
-#I = Identity(2)
-#F_ = I + grad(d)
-#J_ = det(F_)
-
 # Fluid variational form
 F1 = rho_f*((1.0/k)*inner(u - u0, phi) + inner(dot((u - w_), grad(u)), phi))*dx \
     + inner(sigma_fluid(p, u), grad(phi))*dx \
@@ -121,7 +105,6 @@ w_file = File("mvelocity/w.pvd")
 p_file = File("mvelocity/pressure.pvd")
 d_file = File("mvelocity/displacement.pvd")
 
-
 solve(lhs(F2)==rhs(F2), w_, bcs_w)
 u_bc.init(w_)
 
@@ -137,11 +120,12 @@ time_array = np.linspace(0,T,(T/dt)+1)
 flux = []
 while t <= T:
     print "Time: ",t
+
     solve(F1==0, up_, bcs_u,solver_parameters={"newton_solver": \
     {"relative_tolerance": 1E-8,"absolute_tolerance":1E-8,"maximum_iterations":100,"relaxation_parameter":1.0}})
     u,p = up_.split(True)
-    #print "u-w : ", assemble(dot(u-w_,n)*ds(4))
     #plot(u, interactive=True)
+
     flux.append(assemble(dot(u,n)*ds(3)))
     u0.assign(u)
 
@@ -150,7 +134,6 @@ while t <= T:
     #w_file << w_
 
     # To see the mesh move with a give initial w
-    #w_.vector()[:] *= float(k)
     ALE.move(mesh,w_)
     #plot(mesh)#,interactive = True)
 
@@ -158,28 +141,3 @@ while t <= T:
 plt.plot(time,flux);plt.title("Flux, with N = 50, y=0"); plt.ylabel("Flux out");plt.xlabel("Time");plt.grid();
 plt.show()
 # Post processing
-
-
-"""
-#mesh = RectangleMesh(Point(0,0), Point(2, 1), 100, 100, "right")
-
-mesh1 = mesh
-mesh2 = RectangleMesh(Point(0,0), Point(2, 1), 100, 100, "right")
-
-coor1 = mesh1.coordinates()
-
-V1 = VectorFunctionSpace(mesh, "CG", 1)
-V2 = VectorFunctionSpace(mesh, "CG", 1)
-
-u1 = Function(V1)
-u2 = Function(V2)
-w = Function(V1)
-
-for i in range(len(coor1)):
-    # Load u1
-    # Load w to get displacement
-    coor2 = coor2 + displacement
-    u2.vector().set_local(u1.vector().array())
-    u2.vector().apply("insert")
-plot
-"""
