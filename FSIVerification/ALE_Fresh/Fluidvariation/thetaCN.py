2576145c8d5eb24ab2730fc8acff83440be49c9f
Andreas Str|m Slyngstad <andressl@terpsikore.uio.no>
2017-04-04 12:33:14 +0200

    CN shifted scheme

---
+++ FSIVerification/ALE_Fresh/Fluidvariation/thetaCN.py
@@ -0,0 +1,47 @@
+from dolfin import Constant, inner, inv, dot, grad, det, Identity,\
+solve, lhs, rhs, assemble, DirichletBC, div, sym, nabla_grad
+
+
+def F_(U):
+	return Identity(len(U)) + grad(U)
+
+def J_(U):
+	return det(F_(U))
+
+def sigma_f_u(u,d,mu_f):
+    return  mu_f*(grad(u)*inv(F_(d)) + inv(F_(d)).T*grad(u).T)
+
+def sigma_f_p(p, u):
+    return -p*Identity(len(u))
+
+def A_E(J, v, d, rho_f, mu_f, psi, dx_f):
+    return rho_f*inner(J*grad(v)*inv(F_(d))*v, psi)*dx_f \
+        + inner(J*sigma_f_u(v, d, mu_f)*inv(F_(d)).T, grad(psi))*dx_f
+
+
+def fluid_setup(v_, p_, d_, n, psi, gamma, dx_f, ds, mu_f, rho_f, k, v_deg, **semimp_namespace):
+
+    theta = 1./2
+    J_theta = theta*J_(d_["n"]) + (1 - theta)*J_(d_["n-1"])
+
+    A_T = J_theta*rho_f/k*inner(v_["n"] - v_["n-1"], psi)*dx_f \
+        - rho_f*inner(J_(d_["n"])*grad(v_["n"])*inv(F_(d_["n"]))*(((d_["n"]-d_["n-1"])/k)), psi)*dx_f
+
+    A_I = inner(div(J_(d_["n"])*inv(F_(d_["n"]))*v_["n"]), gamma)*dx_f
+
+    A_P = inner(J_(d_["n"])*sigma_f_p(p_["n"], d_["n"])*inv(F_(d_["n"])).T, grad(psi))*dx_f
+
+    F_fluid_nonlinear = A_T + theta*A_E(J_(d_["n"]), v_["n"], d_["n"], rho_f, mu_f, psi, dx_f) \
+                      + A_P + A_I
+
+    F_fluid_linear = (1 - theta)*A_E(J_(d_["n-1"]), v_["n-1"], d_["n-1"], rho_f, mu_f, psi, dx_f) \
+
+    """
+    F_fluid_nonlinear = A_T + theta*A_E(J_(d_["n"]), v_["n"], d_["n"], rho_f, mu_f, psi, dx_f) \
+                   + A_P + A_I \
+                   + (1 - theta)*A_E(J_(d_["n-1"]), v_["n-1"], d_["n-1"], rho_f, mu_f, psi, dx_f)
+
+    F_fluid_linear = inner(Constant((0, 0)), psi)*dx_f
+    """
+
+    return dict(F_fluid_linear = F_fluid_linear, F_fluid_nonlinear = F_fluid_nonlinear)
---
+++ FSIVerification/ALE_Fresh/Structurevariation/thetaCN.py
@@ -0,0 +1,46 @@
+from dolfin import Constant, inner, inv, dot, grad, det, Identity,\
+solve, lhs, rhs, assemble, DirichletBC, div, sym, tr, norm, \
+MPI, mpi_comm_world
+#from semi_implicit import *
+
+def F_(U):
+	return Identity(len(U)) + grad(U)
+
+def J_(U):
+	return det(F_(U))
+
+def E(U):
+	return 0.5*(F_(U).T*F_(U) - Identity(len(U)))
+
+def S(U,lamda_s,mu_s):
+    I = Identity(len(U))
+    return 2*mu_s*E(U) + lamda_s*tr(E(U))*I
+
+def Piola1(U,lamda_s,mu_s):
+	return F_(U)*S(U,lamda_s,mu_s)
+
+def A_E(d, v,  lamda_s, mu_s, rho_s, delta, psi, phi, dx_s):
+	return inner(Piola1(d, lamda_s, mu_s), grad(psi))*dx_s \
+		   - delta*rho_s*inner(v, phi)*dx_s
+
+
+def structure_setup(d_, v_, p_, phi, psi, gamma, dS, mu_f, n,\
+            dx_s, dx_f, mu_s, rho_s, lamda_s, k, mesh_file, **semimp_namespace):
+	theta = 1./2
+	delta = 1
+	#J = theta*J_(d_["n"]) + (1 - theta)*J_(d_["n-1"])
+
+	A_T =  rho_s/k*inner(v_["n"] - v_["n-1"], psi)*dx_s + delta*(rho_s/k)*inner(d_["n"] - d_["n-1"], phi)*dx_s
+
+	F_solid_nonlinear = A_T + theta*A_E(d_["n"], v_["n"], lamda_s, mu_s, rho_s, delta, psi, phi, dx_s) \
+
+	F_solid_linear = (1 - theta)*A_E(d_["n-1"], v_["n-1"], lamda_s, mu_s, rho_s, delta, psi, phi, dx_s)
+
+	"""
+	F_solid_nonlinear = A_T + theta*A_E(d_["n"], v_["n"], lamda_s, mu_s, rho_s, delta, psi, phi, dx_s) \
+					  + (1 - theta)*A_E(d_["n-1"], v_["n-1"], lamda_s, mu_s, rho_s, delta, psi, phi, dx_s)
+
+	F_solid_linear = inner(Constant((0, 0)), psi)*dx_s
+	"""
+
+	return dict(F_solid_linear = F_solid_linear, F_solid_nonlinear = F_solid_nonlinear)
