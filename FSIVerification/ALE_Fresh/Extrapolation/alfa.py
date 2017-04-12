fb169e369064ed2e1194d854e9f339f50d61ac4d
Andreas Strøm Slyngstad <Andreas@eduroam-193-157-117-220.uio.no>
2017-04-12 15:11:44 +0200

    update alfa

--- FSIVerification/ALE_Fresh/Extrapolation/alfa.py
+++ FSIVerification/ALE_Fresh/Extrapolation/alfa.py
@@ -29,10 +29,13 @@ def extrapolate_setup(F_fluid_linear, extype, mesh_file, d_, phi, gamma, dx_f, *
 
     if extype == "linear":
         hmin = mesh_file.hmin()
-        #alfa_lam =  1./(J_(d_["n"]))
-        #alfa_mu = 0.49
+
+        E_y =  1./(J_(d_["n"]))
+        nu = -0.2 #(-1, 0.5)
+        alfa_lam = nu*E_y / ((1 + nu)*(1 - 2*nu))
+        alfa_mu = E_y/(2*(1 + nu))
         alfa_lam = hmin*hmin ; alfa_mu = hmin*hmin
-        print "linear extrapolation !!"
+    
         F_extrapolate = inner(STVK(d_["n"],alfa_mu,alfa_lam) , grad(phi))*dx_f
 
     F_fluid_linear += F_extrapolate
