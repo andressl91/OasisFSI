6eba99d8a21fca313eea25ba6c2438dd1982fb61
Andreas Str|m Slyngstad <andressl@europa.uio.no>
2017-05-13 11:40:50 +0200

    simpler theta cn scheme

--- FSIVerification/ALE_Fresh/Newtonsolver/test.py
+++ FSIVerification/ALE_Fresh/Newtonsolver/test.py
@@ -1,18 +1,19 @@
 from dolfin import *
 from numpy import isnan
-def solver_setup(F_fluid_linear, F_fluid_nonlinear, \
-                F_solid_linear, F_solid_nonlinear, DVP, dvp_, **monolithic):
 
-    print "solver_setup"
+def solver_setup(F_fluid_linear, F_fluid_nonlinear, \
+                 F_solid_linear, F_solid_nonlinear, DVP, dvp_, **monolithic):
     F_lin = F_fluid_linear + F_solid_linear
-    F_nonlin = F_fluid_nonlinear + F_solid_nonlinear
+    F_nonlin = F_solid_nonlinear + F_fluid_nonlinear
     F = F_lin + F_nonlin
 
     chi = TrialFunction(DVP)
     J_linear    = derivative(F_lin, dvp_["n"], chi)
     J_nonlinear = derivative(F_nonlin, dvp_["n"], chi)
 
-    A_pre = assemble(J_linear)
+    #A_pre = assemble(J_linear)
+    A_pre = assemble(J_linear, form_compiler_parameters = \
+                                {"quadtrature_degree": 4})
     A = Matrix(A_pre)
     b = None
 
@@ -22,32 +23,36 @@ def solver_setup(F_fluid_linear, F_fluid_nonlinear, \
 def newtonsolver(F, J_nonlinear, A_pre, A, b, bcs, \
                 dvp_, up_sol, dvp_res, rtol, atol, max_it, T, t, **monolithic):
     Iter      = 0
-    residual   = 1
-    rel_res    = residual
+    residual   = 10**8
+    rel_res    = 10**8
     lmbda = 1
-
+    last_rel_res = residual #Capture if residual increases from last iteration
+    last_residual = rel_res
     while rel_res > rtol and residual > atol and Iter < max_it:
-        #if Iter % 5 == 0:
-        A = assemble(J_nonlinear, tensor=A)# keep_diagonal = True)
-        #A = assemble(J_nonlinear, tensor=A) #keep_diagonal = True
-        A.axpy(1.0, A_pre, True)
-        A.ident_zeros()
+
+        if Iter % 6  == 0 or (last_rel_res < rel_res and last_residual < residual):
+            print "assebmling new JAC"
+            A = assemble(J_nonlinear, tensor=A, form_compiler_parameters =
+                    {"quadrature_degree": 4}, keep_diagonal = True)
+            A.axpy(1.0, A_pre, True)
+            A.ident_zeros()
 
         b = assemble(-F, tensor=b)
 
+        last_rel_res = rel_res #Capture if residual increases from last iteration
+        last_residual = residual
+
         [bc.apply(A, b, dvp_["n"].vector()) for bc in bcs]
         up_sol.solve(A, dvp_res.vector(), b)
-        print type(A)
-        #matrix = as_backend_type(A).mat()
-        #vector = as_backend_type(dvp_res).mat()
-        #t = assemble(J_nonlinear*dvp_res)
         dvp_["n"].vector().axpy(lmbda, dvp_res.vector())
         [bc.apply(dvp_["n"].vector()) for bc in bcs]
         rel_res = norm(dvp_res, 'l2')
         residual = b.norm('l2')
-        if isnan(rel_res) or isnan(residual):
-            print "type rel_res: ",type(rel_res)
-            t = T*T
+        if rel_res > 1E20 or residual > 1E20:
+            print "IN IF TEST"
+            t = T + 1
+            break
+
 
         if MPI.rank(mpi_comm_world()) == 0:
             print "Newton iteration %d: r (atol) = %.3e (tol = %.3e), r (rel) = %.3e (tol = %.3e) " \
