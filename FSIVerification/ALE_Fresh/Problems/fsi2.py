33cdf76b10ac2126d30e11fdbcc9c755df8bd590
Andreas Strøm Slyngstad <Andreas@eduroam-193-157-118-45.uio.no>
2017-06-16 16:37:12 +0200

    updated

--- FSIVerification/ALE_Fresh/Problems/fsi2.py
+++ FSIVerification/ALE_Fresh/Problems/fsi2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import sys
 
 refi = 0
-mesh_file = Mesh("Mesh/fluid_new.xml")
-for i in range(refi):
-    mesh_file = refine(mesh_file)
+#mesh_file = Mesh("Mesh/fluid_new.xml")
+mesh_name = "base0"
+mesh_file = Mesh("Mesh/" + mesh_name +".xml")
 
 common = {"mesh": mesh_file,
           "v_deg": 2,    #Velocity degree
@@ -25,6 +25,7 @@ common = {"mesh": mesh_file,
           "L" : 2.5,
     	  "step" : 1,
           "checkpoint": False  }
+
  #"checkpoint": "./FSI_fresh_checkpoints/FSI-2/P-2/dt-0.05/dvpFile.h5"
 vars().update(common)
 lamda_s = nu_s*2*mu_s/(1 - 2.*nu_s)
@@ -46,7 +47,7 @@ Barwall =  AutoSubDomain(lambda x: "on_boundary" and (( (x[0] - 0.2)*(x[0] - 0.2
 
 Allboundaries = DomainBoundary()
 
-boundaries = FacetFunction("size_t", mesh_file)
+boundaries = FacetFunction("size_t",mesh_file)
 boundaries.set_all(0)
 Allboundaries.mark(boundaries, 1)
 Wall.mark(boundaries, 2)
@@ -100,16 +101,15 @@ else:
     dvp_file=HDF5File(mpi_comm_world(), "FSI_fresh_checkpoints/FSI-2/P-"+str(v_deg)+"/dt-"+str(dt)+"/dvpFile.h5", "w")
 
 
-def initiate(P, v_deg, d_deg, p_deg, dt, theta, dvp_, args, Det_list, refi, **semimp_namespace):
+def initiate(P, v_deg, d_deg, p_deg, dt, theta, dvp_, args, Det_list, refi, mesh_file, mesh_name, **semimp_namespace):
+
     exva = args.extravari
     extype = args.extype
     bitype = args.bitype
     if args.extravari == "alfa":
-        path = "FSI_fresh_results/FSI-2/%(exva)s_%(extype)s/dt-%(dt)g_theta-%(theta)g/refine_%(refi)d_v_deg_%(v_deg)s_d_deg_%(d_deg)s_p_deg_%(p_deg)s" % vars()
+        path = "FSI_fresh_results/FSI-2/%(exva)s_%(extype)s/dt-%(dt)g_theta-%(theta)g/%(mesh_name)s_refine_%(refi)d_v_deg_%(v_deg)s_d_deg_%(d_deg)s_p_deg_%(p_deg)s" % vars()
     if args.extravari == "biharmonic" or args.extravari == "laplace" or args.extravari == "elastic":
-        path = "FSI_fresh_results/FSI-2/%(exva)s_%(bitype)s/dt-%(dt)g_theta-%(theta)g/refine_%(refi)d_v_deg_%(v_deg)s_d_deg_%(d_deg)s_p_deg_%(p_deg)s" % vars()
-        #path = "FSI_fresh_results/FSI-2/"+str(args.extravari)+"_"+ str(args.bitype) +"/dt-"+str(dt)+"_theta-"+str(theta)+"_refine="+str(refi)
-
+        path = "FSI_fresh_results/FSI-2/%(exva)s_%(bitype)s/dt-%(dt)g_theta-%(theta)g/%(mesh_name)s_refine_%(refi)d_v_deg_%(v_deg)s_d_deg_%(d_deg)s_p_deg_%(p_deg)s" % vars()
 
     u_file = XDMFFile(mpi_comm_world(), path + "/velocity.xdmf")
     d_file = XDMFFile(mpi_comm_world(), path + "/d.xdmf")
@@ -122,8 +122,8 @@ def initiate(P, v_deg, d_deg, p_deg, dt, theta, dvp_, args, Det_list, refi, **se
     v = dvp_["n"].sub(1, deepcopy=True)
     p = dvp_["n"].sub(2, deepcopy=True)
     #p_file.write(p)
-    #d_file.write(d)
-    #u_file.write(v)
+    #d_file << d
+    #u_file << v
 
     return dict(u_file=u_file, d_file=d_file, p_file=p_file, path=path)
 
@@ -183,17 +183,23 @@ def pre_solve(t, inlet, **semimp_namespace):
 
 
 def after_solve(t, P, DVP, dvp_, n,coord,dis_x,dis_y,Drag_list,Lift_list,\
-                Det_list, counter,dvp_file,u_file,p_file,d_file,path, **semimp_namespace):
+                Det_list, counter,dvp_file,u_file,p_file,d_file, **semimp_namespace):
 
     d = dvp_["n"].sub(0, deepcopy=True)
     v = dvp_["n"].sub(1, deepcopy=True)
     p = dvp_["n"].sub(2, deepcopy=True)
     #d, v, p = dvp_["n"].split(True)
     if counter%step ==0:
-        u_file << v
-        d_file << d
+        #u_file << v
+        #d_file << d
         #p_file << p
         #p_file.write(p)
+
+        d.rename("d", "displacement")
+        v.rename("v", "velocity")
+        d_file.write(d)
+        u_file.write(v)
+
         #d_file.write(d)
         #u_file.write(v)
         #dvp_file << dvp_["n"]
@@ -212,70 +218,56 @@ def after_solve(t, P, DVP, dvp_, n,coord,dis_x,dis_y,Drag_list,Lift_list,\
     Det = project(J_(d), P)
     Det_list.append((Det.vector().array()).min())
 
+    """
     Dr = -assemble((sigma_f_new(v,p,d,mu_f)*n)[0]*ds(6))
     Li = -assemble((sigma_f_new(v,p,d,mu_f)*n)[1]*ds(6))
     Dr += -assemble((sigma_f_new(v("-"),p("-"),d("-"),mu_f)*n("-"))[0]*dS(5))
     Li += -assemble((sigma_f_new(v("-"),p("-"),d("-"),mu_f)*n("-"))[1]*dS(5))
+    """
+    Dr = -assemble((sigma_f_new(v,p,d,mu_f)*n)[0]*ds(6))
+    Li = -assemble((sigma_f_new(v,p,d,mu_f)*n)[1]*ds(6))
+    Dr += -assemble((sigma_f_new(v("+"),p("+"),d("+"),mu_f)*n("+"))[0]*dS(5))
+    Li += -assemble((sigma_f_new(v("+"),p("+"),d("+"),mu_f)*n("+"))[1]*dS(5))
     Drag_list.append(Dr)
     Lift_list.append(Li)
     Time_list.append(t)
+
+
+    det_func = Function(P)
+    Det = project(J_(d), P)
+    det_func.vector().zero()
+    det_func.vector().axpy(1, Det.vector())
+    Det_list.append((det_func.vector().array()).min())
+
 
     dsx = d(coord)[0]
     dsy = d(coord)[1]
     dis_x.append(dsx)
     dis_y.append(dsy)
-
-
     if MPI.rank(mpi_comm_world()) == 0:
         print "LIFT = %g,  DRAG = %g" % (Li, Dr)
         print "dis_x/dis_y : %g %g "%(dsx,dsy)
-    np.savetxt(path + '/Min_J.txt', Det_list, delimiter=',')
-    np.savetxt(path + '/Lift.txt', Lift_list, delimiter=',')
-    np.savetxt(path + '/Drag.txt', Drag_list, delimiter=',')
-    np.savetxt(path + '/Time.txt', Time_list, delimiter=',')
-    np.savetxt(path + '/dis_x.txt', dis_x, delimiter=',')
-    np.savetxt(path + '/dis_y.txt', dis_y, delimiter=',')
 
     return {}
 
 
 def post_process(path,T,dt,Det_list,dis_x,dis_y, Drag_list,Lift_list, Time_list,\
-    args, DVP, simtime,v_deg, p_deg, d_deg, dvp_file, refi, **semimp_namespace):
+                args, simtime,v_deg, p_deg, d_deg, dvp_file,**semimp_namespace):
     #dvp_file.close()
     #time_list = np.linspace(0,T,T/dt+1)
-
-    #print mean and amplitude values
-    mean = 0.5*(np.max(dis_y[1300:1400]) + np.min(dis_y[1300:1400]))
-    amplitude = 0.5*(np.max(dis_y[1300:1400]) - np.min(dis_y[1300:1400]) )
-    print "dis_y: ",mean, "+", amplitude
-    mean = 0.5*(np.max(dis_x[1300:1400]) + np.min(dis_x[1300:1400]))
-    amplitude = 0.5*(np.max(dis_x[1300:1400]) - np.min(dis_x[1300:1400]) )
-    print "dis_x: ",mean, "+", amplitude
-    mean = 0.5*(np.max(Lift_list[1300:1400]) + np.min(Lift_list[1300:1400]))
-    amplitude = 0.5*(np.max(Lift_list[1300:1400]) - np.min(Lift_list[1300:1400]) )
-    print "Lift: ",mean, "+", amplitude
-    mean = 0.5*(np.max(Drag_list[1300:1400]) + np.min(Drag_list[1300:1400]))
-    amplitude = 0.5*(np.max(Drag_list[1300:1400]) - np.min(Drag_list[1300:1400]) )
-    print "Drag: ",mean, "+", amplitude
-
-
-
     theta = args.theta
     f_scheme = args.fluidvari
     s_scheme = args.solidvari
     e_scheme = args.extravari
-    dofs = DVP.dim()
-    cells = mesh_file.num_cells()
-    if MPI.rank(mpi_comm_world()) == 0:
 
+    if MPI.rank(mpi_comm_world()) == 0:
+        print "IN POSTPRO", path
         f = open(path+"/report.txt", 'w')
         f.write("""FSI2 EXPERIMENT
         T = %(T)g\ndt = %(dt)g\nv_deg = %(d_deg)g\nv_deg = %(v_deg)g\np_deg = %(p_deg)g\n
-    theta = %(theta)s\nf_vari = %(f_scheme)s\ns_vari = %(s_scheme)s\ne_vari = %(e_scheme)s\ntime = %(simtime)g
-    refine = %(refi)s\nDOFS = %(dofs)d\nCELLS = %(cells)d""" % vars())
+    theta = %(theta)s\nf_vari = %(f_scheme)s\ns_vari = %(s_scheme)s\ne_vari = %(e_scheme)s\n time = %(simtime)g""" % vars())
         #f.write("""Runtime = %f """ % fintime)
         f.close()
-
         np.savetxt(path + '/Min_J.txt', Det_list, delimiter=',')
         np.savetxt(path + '/Lift.txt', Lift_list, delimiter=',')
         np.savetxt(path + '/Drag.txt', Drag_list, delimiter=',')
