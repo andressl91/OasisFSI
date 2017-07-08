0131fe6cc4f0fa675fb6b60a09ef33ee587e5d86
Andreas Strøm Slyngstad <Andreas@Andreass-MacBook-Pro.local>
2017-07-08 15:21:14 +0200

    setup for solving CSM2

--- TUREK/CSM/Problems/csm2_b2.py
+++ TUREK/CSM/Problems/csm2_b2.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-mesh_name = "base0"
+mesh_name = "base2"
 mesh_file = Mesh("Mesh/" + mesh_name +".xml")
 #mesh_file = refine(mesh_file)
 #Parameters for each numerical case
@@ -13,7 +13,7 @@ common = {"mesh": mesh_file,
           "T": 10.0,          # End time
           "dt": 0.1,       # Time step
           "rho_s" : Constant(1.0E3),
-          "mu_s" : Constant(0.5E6),
+          "mu_s" : Constant(2.0E6),
           "nu_s" : Constant(0.4),
           "D" : 0.1,
           "H" : 0.41,
@@ -21,7 +21,7 @@ common = {"mesh": mesh_file,
     	  "step" : 1,
           "checkpoint": False
           }
- #"checkpoint": "./FSI_fresh_checkpoints/CSM-1/P-2/dt-0.05/dvFile.h5"
+ #"checkpoint": "./FSI_fresh_checkpoints/CSM-2/P-2/dt-0.05/dvFile.h5"
 g = 2
 vars().update(common)
 lamda_s = nu_s*2*mu_s/(1 - 2.*nu_s)
@@ -59,7 +59,7 @@ def initiate(t, T, args, mesh_file, mesh_name, \
             d_deg, v_deg, dt, dv_, Time_list,**semimp_namespace):
 
     theta = args.theta
-    path = "FSI_fresh_results/CSM-1/dt-%(dt)g_theta-%(theta)g/%(mesh_name)s_d_deg_%(d_deg)s_v_deg_%(v_deg)s" % vars()
+    path = "FSI_fresh_results/CSM-2/dt-%(dt)g_theta-%(theta)g/%(mesh_name)s_d_deg_%(d_deg)s_v_deg_%(v_deg)s" % vars()
     dummy_file=HDF5File(mpi_comm_world(), path + "/dummy.h5", "w")
     d = dv_["n"].sub(0, deepcopy=True)
     Time_list.append(t)
@@ -119,7 +119,7 @@ def post_process(path,T,dt, dis_x,dis_y, Time_list,\
     if MPI.rank(mpi_comm_world()) == 0:
 
         f = open(path+"/report.txt", 'w')
-        f.write("""CSM-1 EXPERIMENT
+        f.write("""CSM-2 EXPERIMENT
         T = %(T)g\ndt = %(dt)g\nv_deg = %(d_deg)g\nv_deg = %(v_deg)g\n
         theta = %(theta)s\n""" % vars())
         #f.write("""Runtime = %f """ % fintime)
---
+++ TUREK/CSM/runs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+python csm.py -T 8 -dt 0.1 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.1 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.1 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+python csm.py -T 8 -dt 0.05 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.05 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.05 -problem csm2 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+python csm.py -T 8 -dt 0.1 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.1 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.1 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+python csm.py -T 8 -dt 0.05 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.05 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.05 -problem csm2_b1 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+python csm.py -T 8 -dt 0.1 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.1 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.1 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+python csm.py -T 8 -dt 0.05 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 1 -d_deg 1
+python csm.py -T 8 -dt 0.05 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 2 -d_deg 2
+python csm.py -T 8 -dt 0.05 -problem csm2_b2 -theta 1.0 -solver reusejac2 -v_deg 3 -d_deg 3
+
+
+
